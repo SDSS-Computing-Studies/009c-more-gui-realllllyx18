6247a0538df41060c391ce56d37e6cae974a84ff
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-11-05 12:01:39 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,23 +9,31 @@ eoutput = StringVar()
 eoutput.set("Here goes thr output")
 
 def function():
-    
-
-
-
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    h = float(h)
+    s = float(s)
+    A = float(A)
+    if a > 0 and b > 0 and c > 0:
+        s = (a+b+c)/2
+        A = math.sqrt(s(s-a)*(s-b)*(s-c))
+    else:
+        A = b*h/2
 
 button1 = tk.Button(window, text="Calculate")
-entry1 = tk.Entry(window, width=10)
+entry1 = tk.Entry(window, width=15)
 triangle = PhotoImage(file="triangle.png")
-entry2 = tk.Entry(window, width=10)
-entry3 = tk.Entry(window, width=10)
+entry2 = tk.Entry(window, width=15)
+entry3 = tk.Entry(window, width=15)
 label1 = tk.Label(window, text="Enter in as much information about the\n triangle shown and I will calcualte the area")
+label2 = tk.Label(window, image=triangle)
 
-button1.grid(row=1, column=1)
-entry1.grid(row=1, column=1)
-triangle.grid(row=1, column=1)
-entry2.grid(row=1, column=1)
-entry3.grid(row=1, column=1)
-lable1.grid(row=1, column=1)
+label2.grid(row=1, column=1, rowspan=1)
+button1.grid(row=5, column=1)
+entry1.grid(row=1, column=0)
+entry2.grid(row=2, column=1)
+entry3.grid(row=3, column=1)
+label1.grid(row=4, column=1)
 
 window.mainloop()
